5942768bca88d0f3a78a60cfb5478997f24caf7e
GwerhDev <gwerh.dev@gmail.com>
2025-04-16 19:46:12 -0400

    styling

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,5 +1,5 @@
 export const API_BASE = import.meta.env.VITE_API_URL;
 export const CLIENT_BASE = import.meta.env.VITE_CLIENT_URL;
 export const CLIENT_NAME = import.meta.env.VITE_CLIENT_NAME;
-export const REDIRECT_LOGIN = import.meta.env.VITE_REDIRECT_URL;
+export const REDIRECT_LOGIN = import.meta.env.VITE_REDIRECT_LOGIN_URL;
 export const REDIRECT_SIGNUP = import.meta.env.VITE_REDIRECT_SIGNUP_URL;
