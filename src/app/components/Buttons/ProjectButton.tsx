b446e0eeacd9c56137c8d97d81225876840004a8
gerardo <gerardo@recemed.com>
2025-04-23 13:13:19 -0400

    adding selected to current project

--- src/app/components/Buttons/ProjectButton.tsx
+++ src/app/components/Buttons/ProjectButton.tsx
@@ -1,27 +1,35 @@
 import s from './ProjectButton.module.css';
 import defaultImg from '../../../assets/streamby-icon.svg';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { setCurrentProject } from '../../../store/currentProjectSlice';
 
 export const ProjectButton = (props: any) => {
   const { project } = props || {};
   const { name, image, id } = project || {};
+  const params = useParams();
   const navigate = useNavigate();
   const dispatch = useDispatch();
+  const currentProjectId = params.id;
 
   const handleOnClick = () => {
     dispatch(setCurrentProject(project));
     navigate(`/project/${id}`);
   };
 
+  const imageContainerClass = currentProjectId === id 
+    ? `${s.imageContainer} ${s.selected}` 
+    : s.imageContainer;
+
   return (
     <button title={name} onClick={handleOnClick} className={s.container}>
-      <span>
+      <span className={imageContainerClass}>
         {
-          image ? <img className={s.image} src={image} alt="" /> : <img className={s.defaultImage} height={30} src={defaultImg} alt="" />
+          image 
+            ? <img className={s.image} src={image} alt="" /> 
+            : <img className={s.defaultImage} height={30} src={defaultImg} alt="" />
         }
       </span>
     </button>
-  )
-}
+  );
+};
