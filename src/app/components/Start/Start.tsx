155b73e78de6f7e3304053c4e04376cc3fd1cdf0
GwerhDev <gwerh.dev@gmail.com>
2025-05-15 00:09:49 -0400

    styling

--- src/app/components/Start/Start.tsx
+++ src/app/components/Start/Start.tsx
@@ -1,10 +1,11 @@
 import s from './Start.module.css';
 import { ActionButton } from '../Buttons/ActionButton';
-import { faPlus } from '@fortawesome/free-solid-svg-icons';
+import { faPlus, faRocket } from '@fortawesome/free-solid-svg-icons';
 import { useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { RootState } from '../../../store';
 import defaultImage from '../../../assets/streamby-icon.svg';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export const Start = () => {
   const projects = useSelector((state: RootState) => state.projects);
@@ -33,17 +34,20 @@ export const Start = () => {
                 projects.map((project: any) => {
                   return (
                     <li key={project.id} onClick={() => navigate('/project/' + project.id + '/dashboard/overview')}>
-                      <span className={s.projectImageContainer}>
-                        <img className={s.projectImage} src={project.image || defaultImage} alt="" />
+                      <span className={s.box}>
+                        <span className={s.projectImageContainer}>
+                          <img className={s.projectImage} src={project.image || defaultImage} alt="" />
+                        </span>
+                        <h5>
+                          {project.name}
+                        </h5>
                       </span>
-                      <h3>
-                        {project.name}
-                      </h3>
+                      <FontAwesomeIcon icon={faRocket} />
                     </li>
-            )
+                  )
                 })
               }
-          </ul>
+            </ul>
           </div>
       }
     </div >
