d285dbad145f5ee1433665171be452335e74a9f5
GwerhDev <gwerh.dev@gmail.com>
2025-07-20 03:54:26 -0400

    adding database options

--- src/app/components/ProjectPresentation/ProjectPresentation.tsx
+++ src/app/components/ProjectPresentation/ProjectPresentation.tsx
@@ -1,13 +1,14 @@
 import s from './ProjectPresentation.module.css';
 import { ActionButton } from '../Buttons/ActionButton';
-import { faEdit } from '@fortawesome/free-solid-svg-icons';
+import { faEdit, faDatabase, faTable } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useSelector } from 'react-redux';
 import { RootState } from '../../../store';
 import { useNavigate } from 'react-router-dom';
 
 export const ProjectPresentation = () => {
   const currentProject = useSelector((state: RootState) => state.currentProject);
-  const { image, name, description } = currentProject || {};
+  const { image, name, description, dbType } = currentProject || {};
   const navigate = useNavigate();
 
   const handleEdit = async () => {
@@ -26,6 +27,10 @@ export const ProjectPresentation = () => {
             }
           </span>
         </li>
+        <li className={s.dbType}>
+          <FontAwesomeIcon icon={dbType === "sql" ? faTable : faDatabase} title={dbType} />
+          <span>{dbType}</span>
+        </li>
       </ul>
       <ul className={s.details}>
         <li className={s.title}>
@@ -35,6 +40,7 @@ export const ProjectPresentation = () => {
         <li>
           <p>{description}</p>
         </li>
+
         <li>
           <span className={s.buttonContainer}>
             <ActionButton onClick={handleEdit} icon={faEdit} text="Edit" type="submit" />
