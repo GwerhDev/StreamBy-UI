5bcc6aa5eee0c70d166313f179875e9216f3c0fb
gerardo <gerardo@recemed.com>
2025-07-24 17:46:45 -0400

    projects are fetched after databases

--- src/hooks/useProjects.ts
+++ src/hooks/useProjects.ts
@@ -3,10 +3,13 @@ import { fetchProjects } from '../services/streamby';
 import { RootState } from '../store';
 import { setProjects } from '../store/projectsSlice';
 import { ProjectList } from '../interfaces';
+import { useEffect } from 'react';
 
 export function useProjects() {
   const dispatch = useDispatch();
   const projectList = useSelector((state: RootState) => state.projects);
+  const { logged } = useSelector((state: RootState) => state.session);
+  const { databases, loading } = useSelector((state: RootState) => state.management);
 
   const loadProjects = (projects: ProjectList[]) => {
     dispatch(setProjects(projects));
@@ -16,6 +19,12 @@ export function useProjects() {
     const newList = await fetchProjects();
     dispatch(setProjects(newList));
   };
+
+  useEffect(() => {
+    if (logged && !loading && databases.length > 0) {
+      refreshProjects();
+    }
+  }, [logged, loading, databases.length]);
 
   return { projectList, loadProjects, refreshProjects };
 }
