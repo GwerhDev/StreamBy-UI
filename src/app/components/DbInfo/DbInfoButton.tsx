b96947e7cdb6fbe293d084825b1f5934a5a0ce31
gerardo <gerardo@recemed.com>
2025-07-24 16:49:50 -0400

    adding database connected icon

--- src/app/components/DbInfo/DbInfoButton.tsx
+++ src/app/components/DbInfo/DbInfoButton.tsx
@@ -3,27 +3,48 @@ import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { RootState } from '../../../store';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faDatabase } from '@fortawesome/free-solid-svg-icons';
+import { faSpinner, faExclamationTriangle, faGlobe } from '@fortawesome/free-solid-svg-icons';
 import { DbInfoModal } from './DbInfoModal';
 
 export const DbInfoButton: React.FC = () => {
-  const { databases } = useSelector((state: RootState) => state.management);
+  const { databases, loading } = useSelector((state: RootState) => state.management);
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   const handleButtonClick = () => {
-    setIsModalOpen(true);
+    setIsModalOpen(!isModalOpen);
   };
 
   const handleCloseModal = () => {
     setIsModalOpen(false);
   };
 
+  const getIcon = () => {
+    if (loading) {
+      return <FontAwesomeIcon icon={faSpinner} spin className={s.icon} />;
+    } else if (databases.length > 0) {
+      return <FontAwesomeIcon icon={faGlobe} className={s.icon} />;
+    } else {
+      return <FontAwesomeIcon icon={faExclamationTriangle} className={s.icon} />;
+    }
+  };
+
+  const getTitle = () => {
+    if (loading) {
+      return "Loading...";
+    } else if (databases.length > 0) {
+      return "Connected";
+    } else {
+      return "Disconnected";
+    }
+  };
+
   return (
     <div className={s.container}>
-      <button title={databases.length ? "Connected" : "Disconnected"} className={s.dbInfoButton} onClick={handleButtonClick}>
-        <FontAwesomeIcon icon={faDatabase} className={s.icon} />
+      <button title={getTitle()} className={s.dbInfoButton} onClick={handleButtonClick}>
+        {getIcon()}
       </button>
-      {isModalOpen && <DbInfoModal databases={databases} />}
+      {isModalOpen && <DbInfoModal databases={databases} onClose={handleCloseModal} />}
     </div>
   );
 };
+
--- src/app/components/DbInfo/DbInfoModal.tsx
+++ src/app/components/DbInfo/DbInfoModal.tsx
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { Database } from '../../../interfaces';
 import s from './DbInfo.module.css';
 
 interface DbInfoModalProps {
   databases: Database[];
+  onClose: () => void;
 }
 
-export const DbInfoModal: React.FC<DbInfoModalProps> = ({ databases }) => {
+export const DbInfoModal: React.FC<DbInfoModalProps> = ({ databases, onClose }) => {
+  const modalRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (modalRef.current && !modalRef.current.contains(event.target as Node)) {
+        onClose();
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [onClose]);
+
   return (
-    <div className={s.modalContent}>
+    <div className={s.modalContent} ref={modalRef}>
       <h4>Connected Databases</h4>
       {databases.length === 0 ? (
         <p>No databases connected.</p>
