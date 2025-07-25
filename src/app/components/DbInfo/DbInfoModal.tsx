f7226c33e3cccdda361c8ceeb0d86b664400fb0b
GwerhDev <gwerh.dev@gmail.com>
2025-07-25 11:07:41 -0400

    styling

--- src/app/components/DbInfo/DbInfoModal.tsx
+++ src/app/components/DbInfo/DbInfoModal.tsx
@@ -1,6 +1,11 @@
-import React, { useEffect, useRef } from 'react';
-import { Database } from '../../../interfaces';
 import s from './DbInfo.module.css';
+import React, { useEffect, useRef } from 'react';
+import { useDispatch } from 'react-redux';
+import { Database } from '../../../interfaces';
+import { fetchDatabases } from '../../../store/managementSlice';
+import { AppDispatch } from '../../../store';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faRefresh } from '@fortawesome/free-solid-svg-icons';
 
 interface DbInfoModalProps {
   databases: Database[];
@@ -9,6 +14,7 @@ interface DbInfoModalProps {
 
 export const DbInfoModal: React.FC<DbInfoModalProps> = ({ databases, onClose }) => {
   const modalRef = useRef<HTMLDivElement>(null);
+  const dispatch = useDispatch<AppDispatch>();
 
   useEffect(() => {
     const handleClickOutside = (event: MouseEvent) => {
@@ -23,16 +29,23 @@ export const DbInfoModal: React.FC<DbInfoModalProps> = ({ databases, onClose }) 
     };
   }, [onClose]);
 
+  const handleRefresh = () => {
+    dispatch(fetchDatabases());
+  };
+
   return (
     <div className={s.modalContent} ref={modalRef}>
-      <h4>Connected Databases</h4>
+      <div className={s.header}>
+        <h4>Connected Databases</h4>
+        <FontAwesomeIcon icon={faRefresh} size='sm' onClick={handleRefresh} className={s.refreshButton} />
+      </div>
       {databases.length === 0 ? (
         <p>No databases connected.</p>
       ) : (
         <ul>
           {databases.map((db) => (
             <li key={db.value}>
-              <strong>{db.name}</strong> ({db.value})
+              <strong>{db.name}</strong> <small>({db.value})</small>
             </li>
           ))}
         </ul>
