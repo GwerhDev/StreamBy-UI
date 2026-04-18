7bd1959b5eb7802ae0a6515a91f9b18c78680156
GwerhDev <gwerh.dev@gmail.com>
2026-04-18 13:23:59 -0400

    fix: aside panel

--- src/app/components/LateralMenu/LateralMenu.tsx
+++ src/app/components/LateralMenu/LateralMenu.tsx
@@ -35,7 +35,7 @@ export const LateralMenu = () => {
     if (isEditorRoute) {
       closeMenu();
     }
-  }, [location, closeMenu]);
+  }, [location]);
 
   const toggleStorage = (value: string) => {
     setExpandedStorages(prev => {
