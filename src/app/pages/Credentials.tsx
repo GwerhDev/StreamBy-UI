1e6c98c51e2ef9127a3730ce2f12900c8248c0fa
gerardo <gerardo@recemed.com>
2026-06-17 10:50:15 -0400

    refactor(pages): simplify Credentials page to thin wrapper

--- src/app/pages/Credentials.tsx
+++ src/app/pages/Credentials.tsx
@@ -1,22 +1,3 @@
-import { useParams } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import { RootState } from '../../store';
 import { CredentialList } from '../components/Credentials/CredentialList';
-import { CreateCredentialForm } from '../components/Forms/CreateCredentialForm';
 
-export const Credentials = () => {
-  const { id: projectId } = useParams<{ id: string }>();
-  const currentProject = useSelector((state: RootState) => state.currentProject.data);
-
-  if (!currentProject || currentProject.id !== projectId) {
-    return <div>Loading project details...</div>;
-  }
-
-  return (
-    <div>
-      <h1>Credentials for {currentProject.name}</h1>
-      <CredentialList />
-      <CreateCredentialForm />
-    </div>
-  );
-};
+export const Credentials = () => <CredentialList />;
